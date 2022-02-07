c1003bc5b05eabf00f9584b0013f44663875f21b
Sima612 <sima101531@yahoo.com>
2022-02-07 10:09:32 -0800

    finished commit

--- move.js
+++ move.js
@@ -5,8 +5,55 @@ function move(element) {
         element.style.left = left + 'px'
         element.style.bottom = bottom + 'px'
     }
+    function moveWithArrowKeys(left, bottom, handleDirectionChange) {
+        let direction = null
+        let x = 100
+        let y = 250
 
+        function moveCharacter() {
+            if (direction === 'west') {
+                x -= 1
+            }
+            if (direction === 'north') {
+                y += 1
+            }
+            if (direction === 'south') {
+                y -= 1
+            }
+            if (direction === 'east') {
+                x += 1
+            }
+            element.style.left = x + 'px'
+            element.style.bottom = y + 'px'
+        }
+        setInterval(moveCharacter, 1) 
+
+        document.addEventListener('keydown', function(e) {
+            if (e.repeat) return;
+
+            if (e.key === 'ArrowLeft') {
+                direction = 'west'
+            }
+            if (e.key === 'ArrowRight') {
+                direction = 'east'
+            }
+            if (e.key === 'ArrowUp') {
+                direction = 'north'
+            }
+            if (e.key === 'ArrowDown') {
+                direction = 'south'
+            }
+            handleDirectionChange(direction)
+        })
+        
+        document.addEventListener('keyup', function(e) {
+            direction = null
+            handleDirectionChange(direction)
+        })
+    }      
+    
     return {
-        to: moveToCoordinates
+        to: moveToCoordinates,
+        withArrowKeys: moveWithArrowKeys
     }
 }
